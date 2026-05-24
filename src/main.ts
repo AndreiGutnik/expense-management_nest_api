2f3cbafeb52e76ba63907827ce28f837e0d21c8c
Andrii Hutnyk <andreigutnik@gmail.com>
2026-05-24 23:14:28 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory, Reflector } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ClassSerializerInterceptor } from '@nestjs/common'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+  app.enableCors()
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+  await app.listen(3000)
+}
+bootstrap()
